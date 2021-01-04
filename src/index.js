aae50757e344f181691608c81abe15b0ffe0bb3b
swathi kunchakuri <swathikunchakuri@swathis-MBP.fritz.box>
2021-01-04 18:44:51 +0000

    navbar hamburger on mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./components/App";
+import "./helpers/navBarHelper.js"
 
 ReactDOM.render(
   <Provider store={store}>
